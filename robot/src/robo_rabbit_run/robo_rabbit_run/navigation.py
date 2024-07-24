42956d622f8681a7b93e7ab4911b13f15cfe5280
tame0001 <thirawat.tam@gmail.com>
2024-07-24 17:49:10 -0400

    sub to odom

--- robot/src/robo_rabbit_run/robo_rabbit_run/navigation.py
+++ robot/src/robo_rabbit_run/robo_rabbit_run/navigation.py
@@ -1,92 +1,35 @@
-import math
-
-from geometry_msgs.msg import Twist
-
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovariance, TwistWithCovariance
 
-from tf2_ros import TransformException
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
 
-
-class FrameListener(Node):
+class NavigationNode(Node):
 
     def __init__(self):
-        super().__init__("navigation")
-
-        # Declare and acquire `target_frame` parameter
-        self.target_frame = (
-            self.declare_parameter("target_frame", "base_link")
-            .get_parameter_value()
-            .string_value
+        super().__init__("scarecrow_nav")
+        self.subscription = self.create_subscription(
+            Odometry,
+            "odom",
+            self.listener_callback,
+            qos_profile=qos_profile_sensor_data,
         )
 
-        self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
+    def listener_callback(self, msg: Odometry):
+        self.extract_msgs(msg.pose, msg.twist)
 
-    # def on_timer(self):
-    #     # Store frame names in variables that will be used to
-    #     # compute transformations
-    #     from_frame_rel = self.target_frame
-    #     to_frame_rel = "turtle2"
-
-    #     if self.turtle_spawning_service_ready:
-    #         if self.turtle_spawned:
-    #             # Look up for the transformation between target_frame and turtle2 frames
-    #             # and send velocity commands for turtle2 to reach target_frame
-    #             try:
-    #                 t = self.tf_buffer.lookup_transform(
-    #                     to_frame_rel, from_frame_rel, rclpy.time.Time()
-    #                 )
-    #             except TransformException as ex:
-    #                 self.get_logger().info(
-    #                     f"Could not transform {to_frame_rel} to {from_frame_rel}: {ex}"
-    #                 )
-    #                 return
-
-    #             msg = Twist()
-    #             scale_rotation_rate = 1.0
-    #             msg.angular.z = scale_rotation_rate * math.atan2(
-    #                 t.transform.translation.y, t.transform.translation.x
-    #             )
-
-    #             scale_forward_speed = 0.5
-    #             msg.linear.x = scale_forward_speed * math.sqrt(
-    #                 t.transform.translation.x**2 + t.transform.translation.y**2
-    #             )
-
-    #             self.publisher.publish(msg)
-    #         else:
-    #             if self.result.done():
-    #                 self.get_logger().info(
-    #                     f"Successfully spawned {self.result.result().name}"
-    #                 )
-    #                 self.turtle_spawned = True
-    #             else:
-    #                 self.get_logger().info("Spawn is not finished")
-    #     else:
-    #         if self.spawner.service_is_ready():
-    #             # Initialize request with turtle name and coordinates
-    #             # Note that x, y and theta are defined as floats in turtlesim/srv/Spawn
-    #             request.name = "turtle2"
-    #             request.x = float(4)
-    #             request.y = float(2)
-    #             request.theta = float(0)
-    #             # Call request
-    #             self.result = self.spawner.call_async(request)
-    #             self.turtle_spawning_service_ready = True
-    #         else:
-    #             # Check if the service is ready
-    #             self.get_logger().info("Service is not ready")
+    def extract_msgs(self, pose: PoseWithCovariance, speed: TwistWithCovariance):
+        print(f"{pose.pose.position.x:4.2f} {pose.pose.position.y:4.2f}")
 
 
-def main():
-    rclpy.init()
-    node = FrameListener()
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-
+def main(args=None):
+    rclpy.init(args=args)
+    node = NavigationNode()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
