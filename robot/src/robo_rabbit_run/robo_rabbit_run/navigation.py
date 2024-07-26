84f207370539f2216b47a3c0631166a8d6d9dd2f
tame0001 <thirawat.tam@gmail.com>
2024-07-25 23:55:25 -0400

    calculate distance

--- robot/src/robo_rabbit_run/robo_rabbit_run/navigation.py
+++ robot/src/robo_rabbit_run/robo_rabbit_run/navigation.py
@@ -1,26 +1,46 @@
+import math
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseWithCovariance, TwistWithCovariance
+from geometry_msgs.msg import PoseWithCovariance, TwistWithCovariance, Point
 
 
 class NavigationNode(Node):
 
     def __init__(self):
+        self.SCALE = (100, 100)  # (x, y) scale factor from meter to pixel
         super().__init__("scarecrow_nav")
-        self.subscription = self.create_subscription(
+        self.odom_sub = self.create_subscription(
             Odometry,
             "odom",
-            self.listener_callback,
+            self.odom_callback,
             qos_profile=qos_profile_sensor_data,
         )
+        self.rabbit_location_sub = self.create_subscription(
+            Point,
+            "rabbit/location",
+            self.rabbit_location_callback,
+            qos_profile=qos_profile_sensor_data,
+        )
+        self.odom = PoseWithCovariance()  # current location
 
-    def listener_callback(self, msg: Odometry):
+    def odom_callback(self, msg: Odometry):
         self.extract_msgs(msg.pose, msg.twist)
 
     def extract_msgs(self, pose: PoseWithCovariance, speed: TwistWithCovariance):
         print(f"{pose.pose.position.x:4.2f} {pose.pose.position.y:4.2f}")
+        self.odom = pose.pose
+
+    def rabbit_location_callback(self, msg: Point):
+        dist = self.calc_distance(msg)
+        print(dist)
+
+    def calc_distance(self, rabbit_location: Point) -> float:
+        return math.sqrt(
+            (self.odom.pose.position.x - rabbit_location.x) ** 2
+            + (self.odom.pose.position.y - rabbit_location.y) ** 2
+        )
 
 
 def main(args=None):
